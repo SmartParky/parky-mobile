1101773766cee5d6e3279c7ae0bff2381f70acc3
Beyza Aksoy <beyzaaksoy@gmail.com>
2018-10-16 00:23:59 +0300

    Header component created.

---
+++ src/common/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, ImageBackground, Dimensions, Image } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+const Header = () => {
+    return(
+        <ImageBackground
+        source={require('../img/header.png')}
+        style={styles.backgroundStyle}>
+        
+         <Image source={require('../img/logo.png')}
+                style={{width:width*0.17, height:height*0.10}} />
+
+        </ImageBackground>
+    );
+};
+
+const styles = {
+    backgroundStyle:{
+        width, 
+        height: height * 0.14, 
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        paddingBottom: 20
+    }
+};
+
+export default Header;
