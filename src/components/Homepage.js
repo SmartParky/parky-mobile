4a380a305b629b2c690045aadb41922b5a4d333c
Beyza Aksoy <beyzaaksoy@gmail.com>
2019-01-19 18:23:43 +0300

    directions to parks on map created.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,21 +1,72 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Alert } from 'react-native';
 import { connect } from 'react-redux';
 
 import RNGooglePlaces from 'react-native-google-places';
 import MapView, { Marker } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
 import { ListPark } from '../actions';
 
 class Homepage extends Component {
     state = {
         region: {
-            latitude: 36.48682975919446,
-            longitude: 36.031581656249955,
+            latitude: 41.0317508,
+            longitude: 40.50566976,
             latitudeDelta: 10,
             longitudeDelta:10,
         },
+        markerPosition: {
+            latitude: 0,
+            longitude: 0,
+            latitudeDelta: 10,
+            longitudeDelta: 10
+        },
+        destination: {
+            latitude:0,
+            longitude:0
+        }
+    }
+
+    watchID = null
+    componentDidMount(){
+        navigator.geolocation.getCurrentPosition((position) => {
+            const lat = parseFloat(position.coords.latitude)
+            const long = parseFloat(position.coords.longitude)
+
+            const initialRegion = {
+                latitude: lat,
+                longitude: long,
+                latitudeDelta: 10,
+                longitudeDelta: 10,
+            }
+
+            this.setState({region: initialRegion })
+            this.setState({markerPosition: initialRegion})
+            
+        }, 
+        (error) => Alert.alert(JSON.stringify(error)),
+        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}) 
+        
+        this.watchID = navigator.geolocation.watchPosition((position) => {
+            const lat = parseFloat(position.coords.latitude)
+            const long = parseFloat(position.coords.longitude)
+
+            const lastRegion = {
+                latitude: lat,
+                longitude: long,
+                latitudeDelta: 10,
+                longitudeDelta: 10,
+            }
+            
+        this.setState({region: lastRegion})
+        this.setState({ markerPosition: lastRegion })
+        })
+    }
+
+    componentWillUnmount(){
+        navigator.geolocation.clearWatch(this.watchID)
     }
     componentWillMount() {
         this.props.ListPark();        
@@ -29,8 +80,9 @@ class Homepage extends Component {
             })
             .catch(error => console.log(error.message));  // error is a Javascript Error object
     }
-    
-    render() {
+  
+    render() {      
+        const GOOGLE_MAPS_APIKEY = 'AIzaSyAXoWKPTU-dgr9SzSFQoH_ts6oPEqW7F2I ';
         const parks = _.map(this.props.parks, park => {
         const park_array = park.coordinate.split(',')
         return( { latitude: parseFloat(park_array[0]), longitude: parseFloat(park_array[1]), id:park.id, name:park.name })
@@ -45,20 +97,65 @@ class Homepage extends Component {
                 <MapView
                     style={{ flex: 1 }}
                     region={this.state.region}
-                >    
-                {console.log(parks)}
-                {_.map(parks, park =>              
-                   <Marker key={park.id} 
-                    coordinate={park}
-                    title={park.name}
-                    />               
-                )}                  
+                >
+                    {_.map(parks, park =>              
+                    <Marker key={park.id} 
+                        coordinate={park}
+                        title={park.name}
+                        onPress={() => this.setState({ destination: {latitude: park.latitude, longitude:park.longitude}}) }
+                        />
+                                 
+                    )}
+                    <Marker
+                    coordinate={this.state.markerPosition} >
+
+                    <View style = {styles.radius}>
+                        <View style = {styles.marker} />
+                    </View>
+        
+                    </Marker>                                                    
+                    <MapViewDirections
+                        origin={this.state.region}
+                        destination={this.state.destination}
+                        apikey={GOOGLE_MAPS_APIKEY}
+                        strokeWidth={6}
+                        strokeColor="hotpink"
+                        onReady={result => {
+                            console.log('Distance:' + result.distance +'km')
+                            console.log('Duration:' + result.duration + 'min')
+                        }}
+                    />                                             
                 </MapView>
+                
             </View>
         );
     }
 }
+const styles = {
+    radius: {
+        height: 50,
+        width: 50,
+        borderRadius: 50 / 2,
+        overflow: "hidden",
+        backgroundColor: "rgba(0, 122, 255, 0.1)",
+        borderWidth: 1,
+        borderColor: "rgba(0, 112, 255, 0.3)",
+        alignItems: "center",
+        justifyContent: "center"
 
+    },
+    marker: {
+        height: 20,
+        width: 20,
+        borderRadius: 20 / 2,
+        overflow: "hidden",
+        backgroundColor: "#007AFF",
+        borderWidth: 3,
+        borderColor: "white",
+        alignItems: "center",
+        justifyContent: "center",
+    }
+}
 const mapToStateProps = ({ rezervationResponse }) => {
     const  {parks} = rezervationResponse;
 
